1505614bcd97763aa06ac4671a367eba5c7acf69
fullpistoladev <mateus_henrique2011@outlook.com>
2022-04-09 17:40:23 -0300

    removendo classe active do menu

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-
 const btnMobile = document.getElementById('btn-mobile');
 
 function toggleMenu() {
   
   const header = document.querySelector("header");
   header.classList.toggle('active');
-  const active = header.classList.contains('active');
   
 }
 
 btnMobile.addEventListener('click', toggleMenu);
+
+
+
+function removerMenu() {
+  const header = document.querySelector("header");
+  header.classList.remove('active');
+}
+
